692982279163b4812fdff2ddda8b7910197240d9
Benton Tripp <benton.tripp@cho.elderresearch.com>
2022-05-19 09:40:32 -0400

    updated medium post style

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,8 @@ smalldesc.style.setProperty('--originalHeight', `${smalldesc.scrollHeight}px`);
 document.querySelector('a').addEventListener('click', function() {
   smalldesc.classList.toggle('expand');
 });
+
+
+// Medium posts - https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js
+
+// https://www.twilik.com/assets/retainable/rss-embed/retainable.js
