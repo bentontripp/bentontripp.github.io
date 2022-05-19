2f4d88c0eb741a71a305257cec1dd4ccbbd5be1a
Benton Tripp <bentontripp@gmail.com>
2022-05-18 22:04:46 -0400

    updated scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 $('#gform').on('submit', function(e) {
     $('#gform *').fadeOut(2000);
-    $('#gform').prepend('Your message has been sent!');
-    document.getElementById("gform").reset();
-    });
+    $('#gform').prepend('Your submission has been processed...');
+});
   
-  function OnReload(event) {
-    document.getElementById("gform").
-  }
-  
-  function OnEnter(event) {
+function OnReload(event) {
+    document.getElementById("gform").reset()
+}  
+
+function OnEnter(event) {
     if (event.keyCode === 13) {
       event.preventDefault();
     }
-  } 
+} 
